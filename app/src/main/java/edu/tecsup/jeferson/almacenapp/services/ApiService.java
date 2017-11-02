9e1e0af657275cf66aabdb9a54de84dffd297031
Jeferson0512 <jeferson.bujaico@tecsup.edu.pe>
2017-11-02 14:14:02 -0500

    Version2

--- app/src/main/java/edu/tecsup/jeferson/almacenapp/services/ApiService.java
+++ app/src/main/java/edu/tecsup/jeferson/almacenapp/services/ApiService.java
@@ -3,8 +3,16 @@ package edu.tecsup.jeferson.almacenapp.services;
 import java.util.List;
 
 import edu.tecsup.jeferson.almacenapp.models.Producto;
+import edu.tecsup.jeferson.almacenapp.models.ResponseMessage;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
 import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
 
 /**
  * Created by Alumno on 10/30/2017.
@@ -16,6 +24,20 @@ public interface ApiService {
 
     @GET("/api/v1/productos")
     Call<List<Producto>> getProductos();
+
+    @FormUrlEncoded
+    @POST("/api/v1/productos")
+    Call<ResponseMessage> createProducto(@Field("nombre") String nombre,
+                                         @Field("precio") String precio,
+                                         @Field("detalles") String detalles);
+    @Multipart
+    @POST("/api/v1/productos")
+    Call<ResponseMessage> createProductoWithImage(
+            @Part("nombre") RequestBody nombre,
+            @Part("precio") RequestBody precio,
+            @Part("detalles") RequestBody detalles,
+            @Part MultipartBody.Part imagen
+    );
 
 
 }
